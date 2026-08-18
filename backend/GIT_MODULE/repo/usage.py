9e40de588a2d911747eef985393a291cde0bd327
atharv-thakre <atharvthakre37@gmail.com>
2026-08-18 16:52:21 +0530

    refactor: update repository usage

--- backend/GIT_MODULE/repo/usage.py
+++ backend/GIT_MODULE/repo/usage.py
@@ -1,39 +1,40 @@
-from repo.repository import Repository
+# from repo.repository import Repository
 
 
-repo = Repository(
-    "https://github.com/dev47929/ThreatLens.git"
-)
-
-
-# Repository information
-print(repo.info_repo())
-
-
-# Latest 10 commits
-commits = repo.list_commits(
-    branch="main",
-    limit=50,
-)
-
-for commit in commits:
-    print(commit)
-
-
-# # Commit metadata
-# commit = repo.info_commit(
-#     commits[0]["sha"]
+# repo = Repository(
+#     "https://github.com/dev47929/ThreatLens.git"
 # )
 
-# print(commit)
+
+# # Repository information
+# print(repo.info_repo())
 
 
-# # Commit diff
-# changes = repo.diff(
-#     commits[0]["sha"]
+
+# # Latest 10 commits
+# commits = repo.list_commits(
+#     branch="main",
+#     limit=50,
 # )
 
-# for change in changes:
-#     print(change["change_type"])
-#     print(change["new_path"])
-#     print(change["diff"])
+# for commit in commits:
+#     print(commit)
+
+
+# # # Commit metadata
+# # commit = repo.info_commit(
+# #     commits[0]["sha"]
+# # )
+
+# # print(commit)
+
+
+# # # Commit diff
+# # changes = repo.diff(
+# #     commits[0]["sha"]
+# # )
+
+# # for change in changes:
+# #     print(change["change_type"])
+# #     print(change["new_path"])
+# #     print(change["diff"])

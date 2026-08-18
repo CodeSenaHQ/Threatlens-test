fce83d6873f9ae601ed195c0526ae58903d166dd
atharv-thakre <atharvthakre37@gmail.com>
2026-08-18 23:30:37 +0530

    refactor: update analysis usage

--- cli-backend/analysis/usage.py
+++ cli-backend/analysis/usage.py
@@ -4,27 +4,28 @@ from repo.repository import Repository
 import json
 
 repo = Repository("https://github.com/dev47929/ThreatLens")
-analysis = CommitAnalyzer(repo)
+# analysis = CommitAnalyzer(repo)
 
-commits = repo.list_commits(
-    branch="main",
-    limit=2,
-)
+# commits = repo.list_commits(
+#     branch="main",
+#     limit=2,
+# )
 
-for commit in commits:
-    result = analysis.analyze(
-        commit["sha"]
-    )
+# for commit in commits:
+#     result = analysis.analyze(
+#         commit["sha"]
+#     )
 
-    print(
-        json.dumps(
-            result,
-            indent=2,
-            default=str,
-        )
-    )
+#     print(
+#         json.dumps(
+#             result,
+#             indent=2,
+#             default=str,
+#         )
+#     )
 
-# analyzer = RepositoryAnalyzer(repo)
+analyzer = RepositoryAnalyzer(repo)
+print(analyzer.analyze().to_json())
 
 # result = analyzer.analyze().to_dict()
 # print(json.dumps(result, indent=2, default=str))

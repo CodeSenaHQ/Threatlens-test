28b91cb8553fd06be91dc14466f85c3da8153a6f
atharv-thakre <atharvthakre37@gmail.com>
2026-08-18 19:15:27 +0530

    feat: initialize local sqlite database

---
+++ cli-backend/db/init.py
@@ -0,0 +1,26 @@
+# db/init.py
+
+from .connection import get_db
+
+
+AUTH_TABLE = """
+CREATE TABLE IF NOT EXISTS auth (
+    id INTEGER PRIMARY KEY CHECK (id = 1),
+    jwt_token TEXT NOT NULL,
+    updated_at INTEGER NOT NULL DEFAULT (unixepoch())
+);
+"""
+
+
+def init_tables():
+    conn = get_db()
+
+    try:
+        conn.execute(AUTH_TABLE)
+        conn.commit()
+    finally:
+        conn.close()
+
+
+def init_db():
+    init_tables()

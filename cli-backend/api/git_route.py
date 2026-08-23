54df9070a7115db5eafba07ef694efe8541a6e48
atharv-thakre <atharvthakre37@gmail.com>
2026-08-23 19:45:07 +0530

    refactor: update git routes

--- cli-backend/api/git_route.py
+++ cli-backend/api/git_route.py
@@ -2,5 +2,7 @@
 from service.repo_service import build_repo, fetch_latest_commit, build_commit_insert , insert_commits
 from repo import Repository
 from db import get_jwt
+from fastapi import APIRouter
 
 
+router = APIRouter()

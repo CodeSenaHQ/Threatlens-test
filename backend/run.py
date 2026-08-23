2a8d272f04161622256766d8ecad2d2e93bf6897
atharv-thakre <atharvthakre37@gmail.com>
2026-08-23 19:45:07 +0530

    refactor: update backend runner

--- backend/run.py
+++ backend/run.py
@@ -1,12 +1,15 @@
 import uvicorn
 
 from connect import app
-from GIT_MODULE.api.repo_route import router
+from GIT_MODULE.api.repo_route import router as repo_router
+from SITE_MODULE.api.chat_route import router as chat_router
+
 
 
 def create_app():
-    print("REGISTERING REPO ROUTER", id(app), id(router))
-    app.include_router(router)
+    print("REGISTERING REPO ROUTER", id(app), id(repo_router), id(chat_router))
+    app.include_router(repo_router)
+    app.include_router(chat_router)
     return app
 
 

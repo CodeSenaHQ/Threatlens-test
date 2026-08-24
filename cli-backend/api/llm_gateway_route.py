e30c8e56102dfe82509cf31a1cf970ebccfb8644
atharv-thakre <atharvthakre37@gmail.com>
2026-08-24 21:15:53 +0530

    Update LLM gateway route

--- cli-backend/api/llm_gateway_route.py
+++ cli-backend/api/llm_gateway_route.py
@@ -1,5 +1,15 @@
-from schema.llm_chat import ChatRequest
-from service.llm_gateway_service import chat_completion
+from schema.llm_chat import ChatRequest , PatchUsageRequest
+from service.llm_gateway_service import chat_completion 
+from db.usage import (
+    get_usage,
+    patch_usage,
+    sync_usage
+)
+
+from db.limit import (
+    get_limit,
+    sync_limit
+)
 from fastapi import APIRouter
 
 
@@ -16,3 +26,24 @@ async def chat_completion_gateway(
     return await chat_completion(body)
 
 
+@router.get("/usage")
+def get_usage_route():
+    return get_usage()
+
+
+@router.patch("/usage")
+def patch_usage_route(body:PatchUsageRequest):
+    return patch_usage(
+        prompt_tokens=body.prompt_tokens,
+        completion_tokens=body.completion_tokens
+    )
+
+
+@router.get("/usage/sync")
+def sync_global_usage():
+    return sync_usage()
+
+
+@router.get("/limit")
+def get_llm_lmits():
+    return get_limit()

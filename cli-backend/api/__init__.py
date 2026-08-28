651d65c817941489405557db20fc15e108a1d43d
atharv-thakre <atharvthakre37@gmail.com>
2026-08-29 01:29:52 +0530

    feat: register SQLi attack router

--- cli-backend/api/__init__.py
+++ cli-backend/api/__init__.py
@@ -5,6 +5,8 @@ from fastapi import APIRouter
 from .auth_route import router as auth_router
 from .chat_route import router as chat_router
 from .git_route import router as git_router
+from .ddos_route import router as ddos_router
+from .sqli_route import router as sqli_router
 from .llm_gateway_route import router as llm_gateway_router
 
 api_router = APIRouter()
@@ -13,4 +15,6 @@ api_router = APIRouter()
 api_router.include_router(auth_router)
 api_router.include_router(chat_router)
 api_router.include_router(git_router)
+api_router.include_router(ddos_router)
+api_router.include_router(sqli_router)
 api_router.include_router(llm_gateway_router)

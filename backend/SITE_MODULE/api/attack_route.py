92691d2a1b5f4c1491df5478c8729341b002e3bc
atharv-thakre <atharvthakre37@gmail.com>
2026-09-01 21:38:23 +0530

    feat: add attack api routes

---
+++ backend/SITE_MODULE/api/attack_route.py
@@ -0,0 +1,52 @@
+from connect import auth
+from fastapi import APIRouter, Depends
+from SITE_MODULE.schema.attack import AttackCreate
+
+from SITE_MODULE.service.attack_service import (
+    post_attack,
+    get_attack
+)
+
+
+router = APIRouter(
+    prefix="/attack",
+    tags=["Attack"],
+)
+
+
+@router.post("")
+def post_attack_route(
+    attack: AttackCreate,
+    user: dict = Depends(auth.deps.get_current),
+):
+    account_id = user["account"]["id"]
+
+    return post_attack(
+        account_id=account_id,
+        **attack.model_dump(),
+    )
+
+
+@router.get("")
+def get_attack_route(
+    attack_type: str = None,
+    page: int = 1,
+    limit: int = 10,
+    user: dict = Depends(auth.deps.get_current),
+):
+    account_id = user["account"]["id"]
+
+    return get_attack(
+        account_id=account_id,
+        attack_type=attack_type,
+        page=page,
+        limit=limit,
+    )
+
+
+import inspect
+
+print("ROUTE FILE:", __file__)
+print("GET_ATTACK:", get_attack)
+print("GET_ATTACK FILE:", inspect.getsourcefile(get_attack))
+print("GET_ATTACK SIGNATURE:", inspect.signature(get_attack))
